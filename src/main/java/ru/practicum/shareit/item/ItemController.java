8bd9deb8041762dcb8fd4ad543a5b3576fd800cb
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2023-12-24 00:54:12 -0800

    refactor: codeStyle изменить именование переменных

--- src/main/java/ru/practicum/shareit/item/ItemController.java
+++ src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -17,39 +17,39 @@ import java.util.List;
 @RequestMapping("/items")
 public class ItemController {
 
-    private final String userIDhead = "X-Sharer-User-Id";
+    private final String userIDHead = "X-Sharer-User-Id";
 
     private final ItemService itemService;
 
     private final ItemMapper itemMapper;
 
     @PostMapping
-    public ItemDto create(@RequestHeader(userIDhead) long userId, @RequestBody ItemDto itemDto) {
+    public ItemDto create(@RequestHeader(userIDHead) long userId, @RequestBody ItemDto itemDto) {
         log.info("Create item");
         return itemService.create(userId, itemMapper.toItem(itemDto, userId));
     }
 
     @PatchMapping("/{itemId}")
-    public ItemDto update(@RequestHeader(userIDhead) long userId, @RequestBody ItemDto itemDto, @PathVariable long itemId) {
+    public ItemDto update(@RequestHeader(userIDHead) long userId, @RequestBody ItemDto itemDto, @PathVariable long itemId) {
         log.info("Update item");
         return itemService.update(userId, itemMapper.toItem(itemDto, userId), itemId);
     }
 
     @GetMapping("/{itemId}")
-    public ItemDto findById(@RequestHeader(userIDhead) long userId, @PathVariable long itemId) {
+    public ItemDto findById(@RequestHeader(userIDHead) long userId, @PathVariable long itemId) {
         log.info("Get information about item");
         return itemService.findById(itemId);
     }
 
     @GetMapping
-    public List<ItemDto> getItemsByUserId(@RequestHeader(userIDhead) long userId) {
+    public List<ItemDto> getItemsByUserId(@RequestHeader(userIDHead) long userId) {
         log.info("Get all user's items");
         return itemService.getItemsByUserId(userId);
     }
 
     @GetMapping("/search")
     @ResponseBody
-    public List<ItemDto> findItems(@RequestHeader(userIDhead) long userId, @RequestParam(defaultValue = "Write the text") String text) {
+    public List<ItemDto> findItems(@RequestHeader(userIDHead) long userId, @RequestParam(defaultValue = "Write the text") String text) {
         log.info("Seach items by request with available status");
         return itemService.findItems(text);
     }
