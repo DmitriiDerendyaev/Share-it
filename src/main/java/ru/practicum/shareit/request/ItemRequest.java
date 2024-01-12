2b3899ad7462ed054726257fff1e5a3d9f19627d
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2024-01-12 10:37:33 -0800

    feature: реализовать маппинг в БД ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -3,19 +3,28 @@ package ru.practicum.shareit.request;
 import lombok.Data;
 import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 /**
  * TODO Sprint add-item-requests.
  */
 @Data
+@Table(name = "requests")
 public class ItemRequest {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "request_id")
     private Long id;
 
+    @Column(name = "description", length = 512)
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
     private User requester;
 
-    private LocalDateTime created;
+    @Column(name = "created")
+    private LocalDate created;
 }
