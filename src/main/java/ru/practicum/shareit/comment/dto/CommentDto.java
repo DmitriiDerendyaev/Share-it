d4603de66f4f11ae8ffb389fd1d95b7a72ffea8e
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2024-01-13 14:21:38 +0300

    feature: добавить работу с комментариями

---
+++ src/main/java/ru/practicum/shareit/comment/dto/CommentDto.java
@@ -0,0 +1,23 @@
+package ru.practicum.shareit.comment.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class CommentDto {
+
+    private long id;
+
+    @NotBlank
+    private String text;
+
+    private long author;
+
+    private String authorName;
+
+    private LocalDateTime created;
+}
