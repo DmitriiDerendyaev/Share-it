725d6c2088a1cd87540aa047e0930dcdafc3dcfc
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2024-01-27 12:38:32 +0300

    feature: имплементировать методы сервиса и контроллера

--- src/main/java/ru/practicum/shareit/request/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestService.java
@@ -1,6 +1,17 @@
 package ru.practicum.shareit.request;
 
+import org.springframework.data.domain.Pageable;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
+
 public interface ItemRequestService {
     public ItemRequest findRequestById(Long userId, Long requestId);
+
+    public ItemRequest addRequest(long userId, ItemRequestDto itemRequestDto);
+
+    public List<ItemRequestDto> getRequest(long userId);
+
+    public List<ItemRequestDto> getAllRequests(long userId, Pageable pageable);
 
 }
--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -1,7 +1,11 @@
 package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -9,5 +13,20 @@ public class ItemRequestServiceImpl implements ItemRequestService {
 
     public ItemRequest findRequestById(Long userId, Long requestId) {
         return new ItemRequest();
+    }
+
+    @Override
+    public ItemRequest addRequest(long userId, ItemRequestDto itemRequestDto) {
+        return null;
+    }
+
+    @Override
+    public List<ItemRequestDto> getRequest(long userId) {
+        return null;
+    }
+
+    @Override
+    public List<ItemRequestDto> getAllRequests(long userId, Pageable pageable) {
+        return null;
     }
 }
