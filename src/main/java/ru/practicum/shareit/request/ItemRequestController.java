7f2e58a1b6603888ea4cb39fc9424a1704a5c186
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2024-01-27 12:35:33 +0300

    feature: исправить нейминг заголовков, добавить контроллер для ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -2,7 +2,14 @@ package ru.practicum.shareit.request;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import java.util.List;
 
 /**
  * TODO Sprint add-item-requests.
@@ -14,10 +21,30 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class ItemRequestController {
 
+    private final String userIDHead = "X-Sharer-User-Id";
     private final ItemRequestService itemRequestService;
 
     @GetMapping("/{requestId}")
-    public ItemRequest findRequestById(@RequestHeader("X-Sharer-User-Id") Long userId, @PathVariable Long requestId) {
+    public ItemRequest findRequestById(@RequestHeader(userIDHead) Long userId, @PathVariable Long requestId) {
         return itemRequestService.findRequestById(userId, requestId);
+    }
+
+    @PostMapping
+    public ItemRequest addRequest(@RequestHeader(userIDHead) long userId, @RequestBody @Valid ItemRequestDto itemRequestDto) {
+        return itemRequestService.addRequest(userId, itemRequestDto);
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> getRequest(@RequestHeader(userIDHead) long userId) {
+
+        return itemRequestService.getRequest(userId);
+    }
+
+    @GetMapping("/all")
+    public List<ItemRequestDto> getAllRequests(@RequestHeader(userIDHead) long userId,
+                                               @RequestParam(defaultValue = "0") @Min(0) Integer from,
+                                               @RequestParam(defaultValue = "10") @Min(1) Integer size) {
+
+        return itemRequestService.getAllRequests(userId, PageRequest.of(from / size, size, Sort.by("created").descending()));
     }
 }
