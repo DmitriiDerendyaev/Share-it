17f1caaf4a81543a8824cb1b773e08b421c370fa
Дерендяев Дмитрий Сергеевич <dimonsterus007@ya.ru>
2024-02-02 23:15:55 +0300

    feature: полное покрытие User

---
+++ src/test/java/ru/practicum/shareit/user/UserRepositoryTest.java
@@ -0,0 +1,39 @@
+package ru.practicum.shareit.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void findByIdTest() {
+        User user = new User(0L, "userName", "email@mail.ru");
+        User savedUser = new User(1L, "userName", "email@mail.ru");
+        userRepository.save(user);
+        User check = userRepository.findById(1L).orElseThrow();
+        assertNotNull(check);
+        assertEquals(check.getId(), savedUser.getId());
+        assertEquals(check.getName(), savedUser.getName());
+    }
+
+    @Test
+    void findAllTest() {
+        User user = new User(0L, "userName", "email@mail.ru");
+        User savedUser = new User(1L, "userName", "email@mail.ru");
+        userRepository.save(user);
+        List<User> check = userRepository.findAll();
+        assertNotNull(check);
+        assertEquals(check.get(0).getId(), savedUser.getId());
+        assertEquals(check.get(0).getName(), savedUser.getName());
+    }
+}
